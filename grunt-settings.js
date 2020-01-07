85d43d9e7169b81aebd72029efb6c9bd1a3b3986
Josh | MC2 Design <josh@mc2design.com>
2020-01-07 15:04:37 -0800

    updated markup to grunt

---
+++ grunt-settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+    sassDir:          'src_assets/scss/',
+    sassMainFileName: 'main',
+    cssDir:           'public_html/assets/css/',
+    cssMainFileDir:   'public_html/assets/css/',
+    cssMainFileName:  'main',
+    jsDir:            'public_html/assets/js/',
+    imgDir:           'public_html/assets/images/',
+    imgSourceDir:     'src_assets/sourceimages/',
+    templatesDir:     'public_html/library/user/templates/',
+    nodeModulesBootstrapDist:     'node_modules/bootstrap/dist/',
+    nodeModulesBootstrapScss:     'node_modules/bootstrap/scss/',
+    //hasBower: {includePaths: ['bower_components/foundation/scss']},
+
+    // sftp server
+    sftpServer:       'example.com',
+    sftpPort:         '2121',
+    sftpLogin:        'login',
+    sftpPas:          'password',
+    sftpDestination:  '/pathTo/css'
+  };
